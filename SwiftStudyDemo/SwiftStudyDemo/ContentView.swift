286d5d49bcba693b56a13d8e88ca94f40d110a07
huangcr <huangcr@baihe.com>
2020-06-17 15:27:35 +0800

    牛逼的Swift

--- SwiftStudyDemo/SwiftStudyDemo/ContentView.swift
+++ SwiftStudyDemo/SwiftStudyDemo/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("你好啊～黄大帅哥!")
+        Text("这么牛逼啊").font(.caption).fontWeight(.light).foregroundColor(Color.yellow).fixedSize(horizontal: true, vertical: false)
     }
 }
 
