087425c222a91fae54458877b616cc37a2df94e4
huangcr <huangcr@baihe.com>
2020-06-17 15:15:12 +0800

    新建SwiftStudyDemo

---
+++ SwiftStudyDemo/SwiftStudyDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftStudyDemo
+//
+//  Created by 黄成瑞 on 2020/6/17.
+//  Copyright © 2020 黄成瑞. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
